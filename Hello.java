aa2b50bfe1d24bb11f7cd6a8372aab781f311d64
Masud Mohamoud <masudmohamoud@Masuds-MacBook-Air.local>
2025-02-27 15:50:39 +0000

    Add mainBranchMethod to Hello.java in main branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
     public static void main(String [] args){
-        System.out.println("Hello World");
+        System.out.println("Hello from main branch!");
     }
+    public void mainBranchMethod(){
+        System.out.println("This is a method from main branch");
+    } 
 }
